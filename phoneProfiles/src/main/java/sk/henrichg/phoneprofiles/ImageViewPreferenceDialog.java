0695515383ef2a9841278bba75b0d88517c29e58
henrichg <henrich.gron@chello.sk>
2015-03-25 13:49:24 +0100

    ImageViewPreference as MaterialDialog

--- phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/ImageViewPreferenceDialog.java
+++ phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/ImageViewPreferenceDialog.java
@@ -1,80 +1,59 @@
 package sk.henrichg.phoneprofiles;
 
-import android.app.Dialog;
 import android.content.Context;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.Button;
 import android.widget.GridView;
 
-//import android.preference.Preference;
-//import android.preference.Preference.OnPreferenceChangeListener;
+import com.afollestad.materialdialogs.MaterialDialog;
 
+public class ImageViewPreferenceDialog  {
 
-public class ImageViewPreferenceDialog extends Dialog {
+    private ImageViewPreference imageViewPreference;
+    private String imageSource;
+    MaterialDialog dialog;
 
-	private ImageViewPreference imageViewPreference;
-	private String imageSource;
-	
-	public ImageViewPreferenceDialog(Context context) {
-		super(context);
-	}
-	
-	public ImageViewPreferenceDialog(Context context, ImageViewPreference preference, String imgSource, 
-										String imageIdentifier, boolean isImageResourceID)
-	{
-		super(context);
+    public ImageViewPreferenceDialog(Context context, ImageViewPreference preference, String imgSource,
+                                     String imageIdentifier, boolean isImageResourceID)
+    {
+        this.imageViewPreference = preference;
+        this.imageSource = imgSource;
 
-		imageViewPreference = preference;
-		imageSource = imgSource;
+        MaterialDialog.Builder dialogBuilder = new MaterialDialog.Builder(context)
+                .title(R.string.title_activity_image_view_preference_dialog)
+                .autoDismiss(false)
+                .customView(R.layout.activity_imageview_resource_pref_dialog, false);
 
-		final Context _context = context;
-		
-		GridView gridView;
-		
-		if (imageSource.equals("resource_file"))
-		{
-			setContentView(R.layout.activity_imageview_resource_file_pref_dialog);
-			gridView = (GridView)findViewById(R.id.imageview_resource_file_pref_dlg_gridview);
-		}
-		else
-		{
-			setContentView(R.layout.activity_imageview_resource_pref_dialog);
-			gridView = (GridView)findViewById(R.id.imageview_resource_pref_dlg_gridview);
-		}
-		
-		gridView.setAdapter(new ImageViewPreferenceAdapter(_context, imageIdentifier, isImageResourceID));
-	
-		gridView.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-				imageViewPreference.setImageIdentifierAndType(ImageViewPreferenceAdapter.ThumbsIds[position], true);
-				ImageViewPreferenceDialog.this.dismiss();
-			}
+        if (imageSource.equals("resource_file"))
+        {
+            dialogBuilder.positiveText(R.string.imageview_resource_file_pref_dialog_gallery_btn);
+            dialogBuilder.callback(new MaterialDialog.ButtonCallback() {
+                @Override
+                public void onPositive(MaterialDialog dialog) {
+                    // zavolat galeriu na vyzdvihnutie image
+                    imageViewPreference.startGallery();
+                    dialog.dismiss();
+                }
+            });
+        }
 
-		});
+        dialog = dialogBuilder.build();
 
-		if (imageSource.equals("resource_file"))
-		{
-			Button button = (Button)findViewById(R.id.imageview_resource_file_pref_dlg_button);
-			button.setOnClickListener(new View.OnClickListener() {
+        GridView gridView = (GridView)dialog.getCustomView().findViewById(R.id.imageview_resource_pref_dlg_gridview);
+        gridView.setAdapter(new ImageViewPreferenceAdapter(context, imageIdentifier, isImageResourceID));
 
-				public void onClick(View v) {
-					// zavolat galeriu na vyzdvihnutie image
-					imageViewPreference.startGallery();
-					ImageViewPreferenceDialog.this.dismiss();
-				}
+        gridView.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                imageViewPreference.setImageIdentifierAndType(ImageViewPreferenceAdapter.ThumbsIds[position], true);
+                dialog.dismiss();
+            }
 
-			});
-		}
-		
-/*		imageViewPreference.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
-			public boolean onPreferenceChange(Preference preference, Object newValue) {
-			}	
-		}); */
-		
-			
-	}
+        });
 
+    }
 
+    public void show() {
+        dialog.show();
+    }
 }
