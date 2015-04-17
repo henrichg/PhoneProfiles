44ab3f383343366b910d836dad03626b1c192409
henrichg <henrich.gron@chello.sk>
2015-04-17 19:09:14 +0200

    Fixed dual SIM support for call state receiver

--- phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/PhoneCallReceiver.java
+++ phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/PhoneCallReceiver.java
@@ -1,17 +1,17 @@
-package sk.henrichg.phoneprofiles;
+package sk.henrichg.phoneprofilesplus;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.telephony.PhoneStateListener;
+import android.support.v4.content.WakefulBroadcastReceiver;
 import android.telephony.TelephonyManager;
 
 import java.util.Date;
 
-public abstract class PhoneCallReceiver extends BroadcastReceiver {
+public abstract class PhoneCallReceiver extends WakefulBroadcastReceiver {
 
-	static TelephonyManager telephony;
-    //The receiver will be recreated whenever android feels like it.  We need a static variable to remember data between instantiations
+    static TelephonyManager telephony;
+    //The receiver will be recreated whenever android feels like it.
+    //We need a static variable to remember data between instantiations
     static PhonecallStartEndDetector listener;
     //String outgoingSavedNumber;
     protected Context savedContext;
@@ -20,28 +20,28 @@ public abstract class PhoneCallReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         savedContext = context;
-        
+
         if (onStartReceive())
         {
-        	if (telephony == null)
-        		telephony = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE); 
-        	
-	        if(listener == null){
-	            listener = new PhonecallStartEndDetector();
-	        }
+            if (telephony == null)
+                telephony = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
 
-	        //We listen to two intents.  The new outgoing call only tells us of an outgoing call.  We use it to get the number.
-	        if (intent.getAction().equals("android.intent.action.NEW_OUTGOING_CALL")) {
-	            listener.setOutgoingNumber(intent.getExtras().getString("android.intent.extra.PHONE_NUMBER"));
-	            return;
-	        }
-	
-	        //The other intent tells us the phone state changed.  Here we set a listener to deal with it
-	        telephony.listen(listener, PhoneStateListener.LISTEN_CALL_STATE);
+            if(listener == null){
+                listener = new PhonecallStartEndDetector();
+            }
+
+            //We listen to two intents.  The new outgoing call only tells us of an outgoing call.  We use it to get the number.
+            if (intent.getAction().equals(Intent.ACTION_NEW_OUTGOING_CALL)) {
+                listener.setOutgoingNumber(intent.getExtras().getString(Intent.EXTRA_PHONE_NUMBER));
+                return;
+            }
+
+            listener.onCallStateChanged(intent);
+
         }
         onEndReceive();
     }
-        
+
 
     //Derived classes should override these to respond to specific events of interest
     protected abstract boolean onStartReceive();
@@ -49,13 +49,13 @@ public abstract class PhoneCallReceiver extends BroadcastReceiver {
     protected abstract void onOutgoingCallStarted(String number, Date start);
     protected abstract void onOutgoingCallAnswered(String number, Date start);
     protected abstract void onIncomingCallAnswered(String number, Date start);
-    protected abstract void onIncomingCallEnded(String number, Date start, Date end); 
+    protected abstract void onIncomingCallEnded(String number, Date start, Date end);
     protected abstract void onOutgoingCallEnded(String number, Date start, Date end);
     protected abstract void onMissedCall(String number, Date start);
     protected abstract void onEndReceive();
 
     //Deals with actual events
-    public class PhonecallStartEndDetector extends PhoneStateListener {
+    public class PhonecallStartEndDetector {
         int lastState = TelephonyManager.CALL_STATE_IDLE;
         Date callStartTime;
         boolean inCall;
@@ -66,7 +66,7 @@ public abstract class PhoneCallReceiver extends BroadcastReceiver {
 
         //The outgoing number is only sent via a separate intent, so we need to store it out of band
         public void setOutgoingNumber(String number){
-        	inCall = false;
+            inCall = false;
             isIncoming = false;
             savedNumber = number;
             callStartTime = new Date();
@@ -75,35 +75,35 @@ public abstract class PhoneCallReceiver extends BroadcastReceiver {
 
         //Incoming call-  goes from IDLE to RINGING when it rings, to OFFHOOK when it's answered, to IDLE when its hung up
         //Outgoing call-  goes from IDLE to OFFHOOK when it dials out, to IDLE when hung up
-        @Override
-        public void onCallStateChanged(int state, String incomingNumber) {
-            super.onCallStateChanged(state, incomingNumber);
+        public void onCallStateChanged(Intent intent) {
+            int state = telephony.getCallState();
             if(lastState == state){
                 //No change, debounce extras
                 return;
             }
             switch (state) {
                 case TelephonyManager.CALL_STATE_RINGING:
-                	inCall = false;
+                    inCall = false;
                     isIncoming = true;
                     callStartTime = new Date();
+                    String incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
                     savedNumber = incomingNumber;
                     onIncomingCallStarted(incomingNumber, callStartTime);
                     break;
                 case TelephonyManager.CALL_STATE_OFFHOOK:
                     //Transition of ringing->offhook are pickups of incoming calls.  Nothing donw on them
                     if(lastState != TelephonyManager.CALL_STATE_RINGING){
-                    	inCall = true;
+                        inCall = true;
                         isIncoming = false;
                         callStartTime = new Date();
-                        onOutgoingCallAnswered(savedNumber, callStartTime);                      
+                        onOutgoingCallAnswered(savedNumber, callStartTime);
                     }
                     else
                     {
-                    	inCall = true;
+                        inCall = true;
                         isIncoming = true;
                         callStartTime = new Date();
-                    	onIncomingCallAnswered(savedNumber, callStartTime);
+                        onIncomingCallAnswered(savedNumber, callStartTime);
                     }
                     break;
                 case TelephonyManager.CALL_STATE_IDLE:
@@ -112,22 +112,19 @@ public abstract class PhoneCallReceiver extends BroadcastReceiver {
                         //Ring but no pickup-  a miss
                         onMissedCall(savedNumber, callStartTime);
                     }
-                    else 
+                    else
                     {
-	                    if(isIncoming){
-	                        onIncomingCallEnded(savedNumber, callStartTime, new Date());
-	                    }
-	                    else{
-	                        onOutgoingCallEnded(savedNumber, callStartTime, new Date());
-	                    }
-                    	inCall = false;
+                        if(isIncoming){
+                            onIncomingCallEnded(savedNumber, callStartTime, new Date());
+                        }
+                        else{
+                            onOutgoingCallEnded(savedNumber, callStartTime, new Date());
+                        }
+                        inCall = false;
                     }
                     break;
             }
             lastState = state;
-
-	        telephony.listen(listener, PhoneStateListener.LISTEN_NONE);
-            
         }
 
     }
