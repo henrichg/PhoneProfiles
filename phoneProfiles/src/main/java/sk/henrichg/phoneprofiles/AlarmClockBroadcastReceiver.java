bef723d22d616a7eb78937c147d88a917ee94674
henrichg <henrich.gron@chello.sk>
2015-07-31 15:35:46 +0200

    Imports optimization

--- phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/AlarmClockBroadcastReceiver.java
+++ phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/AlarmClockBroadcastReceiver.java
@@ -4,8 +4,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.provider.Settings;
-import android.service.notification.NotificationListenerService;
-import android.util.Log;
 
 public class AlarmClockBroadcastReceiver extends BroadcastReceiver {
     public AlarmClockBroadcastReceiver() {
