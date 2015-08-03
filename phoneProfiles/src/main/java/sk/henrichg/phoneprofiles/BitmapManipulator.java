b4e293623c3f6f18e1322e1d61937a0156d10781
henrichg <henrich.gron@chello.sk>
2015-08-03 20:52:13 +0200

    Fixed FC

--- phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/BitmapManipulator.java
+++ phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/BitmapManipulator.java
@@ -16,92 +16,95 @@ import android.graphics.PorterDuffColorFilter;
 import java.io.File;
 
 public class BitmapManipulator {
-	
-	public static Bitmap resampleBitmap(String bitmapFile, int width, int height)
-	{
-		File f = new File(bitmapFile);
-		if (f.exists())
-		{
-			// first decode with inJustDecodeDpunds=true to check dimensions
-			final BitmapFactory.Options options = new BitmapFactory.Options();
-			options.inJustDecodeBounds = true;
-			BitmapFactory.decodeFile(bitmapFile, options);
-			// calaculate inSampleSize
-			options.inSampleSize = calculateInSampleSize(options, width, height);
-			// decode bitmap with inSampleSize
-			options.inJustDecodeBounds = false;
-			Bitmap decodedSampleBitmap = BitmapFactory.decodeFile(bitmapFile, options);
-			
-			return decodedSampleBitmap;
-		}
-		else
-			return null;
-	}
 
-	public static Bitmap recolorBitmap(Bitmap bitmap, int color, Context context)
-	{
-		if (bitmap == null)
-			return null;
+    public static Bitmap resampleBitmap(String bitmapFile, int width, int height)
+    {
+        if (bitmapFile == null)
+            return null;
 
-		Bitmap colorBitmap = Bitmap.createBitmap(bitmap.getWidth(),
-				bitmap.getHeight(),
-				bitmap.getConfig());
-		//Config.ARGB_8888);
+        File f = new File(bitmapFile);
+        if (f.exists())
+        {
+            // first decode with inJustDecodeDpunds=true to check dimensions
+            final BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            BitmapFactory.decodeFile(bitmapFile, options);
+            // calaculate inSampleSize
+            options.inSampleSize = calculateInSampleSize(options, width, height);
+            // decode bitmap with inSampleSize
+            options.inJustDecodeBounds = false;
+            Bitmap decodedSampleBitmap = BitmapFactory.decodeFile(bitmapFile, options);
 
-		Canvas canvas = new Canvas(colorBitmap);
-		Paint paint = new Paint();
-		Matrix matrix = new Matrix();
+            return decodedSampleBitmap;
+        }
+        else
+            return null;
+    }
 
-		ColorFilter filter = new PorterDuffColorFilter(color, PorterDuff.Mode.SRC_ATOP);
-		paint.setColorFilter(filter);
-		canvas.drawBitmap(bitmap, matrix, paint);
+    public static Bitmap recolorBitmap(Bitmap bitmap, int color, Context context)
+    {
+        if (bitmap == null)
+            return null;
 
-		return colorBitmap;
-	}
+        Bitmap colorBitmap = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(),
+                bitmap.getConfig());
+        //Config.ARGB_8888);
 
-	public static Bitmap monochromeBitmap(Bitmap bitmap, int value, Context context) {
-		int color = Color.argb(0xFF, value, value, value);
-		return recolorBitmap(bitmap, color, context);
-	}
+        Canvas canvas = new Canvas(colorBitmap);
+        Paint paint = new Paint();
+        Matrix matrix = new Matrix();
 
-	public static Bitmap grayscaleBitmap(Bitmap bitmap)
-	{
-		if (bitmap == null)
-			return null;
+        ColorFilter filter = new PorterDuffColorFilter(color, PorterDuff.Mode.SRC_ATOP);
+        paint.setColorFilter(filter);
+        canvas.drawBitmap(bitmap, matrix, paint);
 
-		Bitmap monochromeBitmap = Bitmap.createBitmap(bitmap.getWidth(),
-													bitmap.getHeight(),
-													bitmap.getConfig());
-    	Canvas canvas = new Canvas(monochromeBitmap);
-    	Paint paint = new Paint();
-    	ColorMatrix colorMatrix = new ColorMatrix();
-    	colorMatrix.setSaturation(0.0f);
-    	paint.setColorFilter(new ColorMatrixColorFilter(colorMatrix));
-    	Matrix matrix = new Matrix();
-    	canvas.drawBitmap(bitmap, matrix, paint);
+        return colorBitmap;
+    }
 
-    	return monochromeBitmap;
-	}
-	
-	private static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight)
-	{
-		// raw height and width of image
-		final int height = options.outHeight;
-		final int width = options.outWidth;
-		int inSampleSize = 1;
-		
-		if (height > reqHeight || width > reqWidth)
-		{
-			// calculate ratios of height and width to requested height an width
-			final int heightRatio = Math.round((float) height / (float) reqHeight);
-			final int widthRatio = Math.round((float) width / (float) reqWidth);
-			
-			// choose the smalest ratio as InSamleSize value, this will guarantee
-			// a final image with both dimensions larger than or equal to the
-			// requested height and width
-			inSampleSize = (heightRatio < widthRatio) ? heightRatio : widthRatio;
-		}
-		return inSampleSize;
-	}
-	
+    public static Bitmap monochromeBitmap(Bitmap bitmap, int value, Context context) {
+        int color = Color.argb(0xFF, value, value, value);
+        return recolorBitmap(bitmap, color, context);
+    }
+
+    public static Bitmap grayscaleBitmap(Bitmap bitmap)
+    {
+        if (bitmap == null)
+            return null;
+
+        Bitmap monochromeBitmap = Bitmap.createBitmap(bitmap.getWidth(),
+                                                    bitmap.getHeight(),
+                                                    bitmap.getConfig());
+        Canvas canvas = new Canvas(monochromeBitmap);
+        Paint paint = new Paint();
+        ColorMatrix colorMatrix = new ColorMatrix();
+        colorMatrix.setSaturation(0.0f);
+        paint.setColorFilter(new ColorMatrixColorFilter(colorMatrix));
+        Matrix matrix = new Matrix();
+        canvas.drawBitmap(bitmap, matrix, paint);
+
+        return monochromeBitmap;
+    }
+
+    private static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight)
+    {
+        // raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth)
+        {
+            // calculate ratios of height and width to requested height an width
+            final int heightRatio = Math.round((float) height / (float) reqHeight);
+            final int widthRatio = Math.round((float) width / (float) reqWidth);
+
+            // choose the smalest ratio as InSamleSize value, this will guarantee
+            // a final image with both dimensions larger than or equal to the
+            // requested height and width
+            inSampleSize = (heightRatio < widthRatio) ? heightRatio : widthRatio;
+        }
+        return inSampleSize;
+    }
+
 }
--- phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/ShortcutCreatorListFragment.java
+++ phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/ShortcutCreatorListFragment.java
@@ -23,87 +23,87 @@ import java.util.List;
 
 public class ShortcutCreatorListFragment extends Fragment {
 
-	private DataWrapper dataWrapper;
-	private List<Profile> profileList;
-	private ShortcutProfileListAdapter profileListAdapter;
-	private ListView listView;
-	
-	private WeakReference<LoadProfileListAsyncTask> asyncTaskContext;
-	
-	public ShortcutCreatorListFragment() {
-	}
-	
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
+    private DataWrapper dataWrapper;
+    private List<Profile> profileList;
+    private ShortcutProfileListAdapter profileListAdapter;
+    private ListView listView;
 
-		super.onCreate(savedInstanceState);
+    private WeakReference<LoadProfileListAsyncTask> asyncTaskContext;
 
-		// this is really important in order to save the state across screen
-		// configuration changes for example
-		setRetainInstance(true);
-	
-		dataWrapper = new DataWrapper(getActivity().getApplicationContext(), true, false, 0);
-		
-	}
-	
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View rootView;
-		
-		rootView = inflater.inflate(R.layout.shortcut_creator_list, container, false); 
+    public ShortcutCreatorListFragment() {
+    }
 
-		return rootView;
-	}
-	
-	@Override
-	public void onViewCreated(View view, Bundle savedInstanceState) {
-		super.onViewCreated(view, savedInstanceState);
-		doOnViewCreated(view, savedInstanceState);
-	}
-	
-	//@Override
-	public void doOnViewCreated(View view, Bundle savedInstanceState)
-	{
-		listView = (ListView)view.findViewById(R.id.shortcut_profiles_list);
-		
-		listView.setOnItemClickListener(new OnItemClickListener() {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
 
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        super.onCreate(savedInstanceState);
 
-				createShortcut(position);
+        // this is really important in order to save the state across screen
+        // configuration changes for example
+        setRetainInstance(true);
 
-			}
-			
-		});
+        dataWrapper = new DataWrapper(getActivity().getApplicationContext(), true, false, 0);
 
-		if (profileList == null)
-		{
-			LoadProfileListAsyncTask asyncTask = new LoadProfileListAsyncTask(this);
-		    this.asyncTaskContext = new WeakReference<LoadProfileListAsyncTask >(asyncTask );
-		    asyncTask.execute();			
-		}
-		else
-		{
-			listView.setAdapter(profileListAdapter);
-		}
-		
-	}
-	
-	private static class LoadProfileListAsyncTask extends AsyncTask<Void, Void, Void> {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView;
+
+        rootView = inflater.inflate(R.layout.shortcut_creator_list, container, false);
+
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        doOnViewCreated(view, savedInstanceState);
+    }
+
+    //@Override
+    public void doOnViewCreated(View view, Bundle savedInstanceState)
+    {
+        listView = (ListView)view.findViewById(R.id.shortcut_profiles_list);
+
+        listView.setOnItemClickListener(new OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                createShortcut(position);
+
+            }
+
+        });
+
+        if (profileList == null)
+        {
+            LoadProfileListAsyncTask asyncTask = new LoadProfileListAsyncTask(this);
+            this.asyncTaskContext = new WeakReference<LoadProfileListAsyncTask >(asyncTask );
+            asyncTask.execute();
+        }
+        else
+        {
+            listView.setAdapter(profileListAdapter);
+        }
+
+    }
+
+    private static class LoadProfileListAsyncTask extends AsyncTask<Void, Void, Void> {
 
         private WeakReference<ShortcutCreatorListFragment> fragmentWeakRef;
-		private DataWrapper dataWrapper;
+        private DataWrapper dataWrapper;
 
-		private class ProfileComparator implements Comparator<Profile> {
-			public int compare(Profile lhs, Profile rhs) {
-				int res = GUIData.collator.compare(lhs._name, rhs._name);
-				return res;
-			}
-		}
+        private class ProfileComparator implements Comparator<Profile> {
+            public int compare(Profile lhs, Profile rhs) {
+                int res = GUIData.collator.compare(lhs._name, rhs._name);
+                return res;
+            }
+        }
 
         private LoadProfileListAsyncTask (ShortcutCreatorListFragment fragment) {
             this.fragmentWeakRef = new WeakReference<ShortcutCreatorListFragment>(fragment);
-	        this.dataWrapper = new DataWrapper(fragment.getActivity().getApplicationContext(), true, false, 0);
+            this.dataWrapper = new DataWrapper(fragment.getActivity().getApplicationContext(), true, false, 0);
         }
 
         @Override
@@ -120,94 +120,93 @@ public class ShortcutCreatorListFragment extends Fragment {
             ShortcutCreatorListFragment fragment = this.fragmentWeakRef.get(); 
             
             if ((fragment != null) && (fragment.isAdded())) {
-            	
-    	        // get local profileList
-    	    	List<Profile> profileList = dataWrapper.getProfileList();
-    	    	// set copy local profile list into activity profilesDataWrapper
-    	        fragment.dataWrapper.setProfileList(profileList, false);
-    	        // set reference of profile list from profilesDataWrapper
-    	        fragment.profileList = fragment.dataWrapper.getProfileList();
 
-    	        fragment.profileListAdapter = new ShortcutProfileListAdapter(fragment, fragment.profileList);
-    	        fragment.listView.setAdapter(fragment.profileListAdapter);
+                // get local profileList
+                List<Profile> profileList = dataWrapper.getProfileList();
+                // set copy local profile list into activity profilesDataWrapper
+                fragment.dataWrapper.setProfileList(profileList, false);
+                // set reference of profile list from profilesDataWrapper
+                fragment.profileList = fragment.dataWrapper.getProfileList();
+
+                fragment.profileListAdapter = new ShortcutProfileListAdapter(fragment, fragment.profileList);
+                fragment.listView.setAdapter(fragment.profileListAdapter);
             }
         }
     }
-	
-	private boolean isAsyncTaskPendingOrRunning() {
-	    return this.asyncTaskContext != null &&
-	          this.asyncTaskContext.get() != null && 
-	          !this.asyncTaskContext.get().getStatus().equals(AsyncTask.Status.FINISHED);
-	}	
-	
-	@Override
-	public void onStart()
-	{
-		super.onStart();
-	}
-	
-	@Override
-	public void onDestroy()
-	{
-		if (!isAsyncTaskPendingOrRunning())
-		{
-			if (listView != null)
-				listView.setAdapter(null);
-			if (profileListAdapter != null)
-				profileListAdapter.release();
-			
-			profileList = null;
-	
-			if (dataWrapper != null)
-				dataWrapper.invalidateDataWrapper();
-			dataWrapper = null;
-		}
-		
-		super.onDestroy();
-	}
 
-	private void createShortcut(int position)
-	{
-		Profile profile = profileList.get(position);
-		boolean isIconResourceID;
-		String iconIdentifier;
-		Bitmap profileBitmap;
-		Bitmap shortcutOverlayBitmap;
-		Bitmap profileShortcutBitmap;
-		String profileName;
-		boolean useCustomColor;
+    private boolean isAsyncTaskPendingOrRunning() {
+        return this.asyncTaskContext != null &&
+              this.asyncTaskContext.get() != null &&
+              !this.asyncTaskContext.get().getStatus().equals(AsyncTask.Status.FINISHED);
+    }
 
-		if (profile != null)
-		{
-			isIconResourceID = profile.getIsIconResourceID();
-			iconIdentifier = profile.getIconIdentifier();
-			profileName = profile._name;
-			useCustomColor = profile.getUseCustomColorForIcon();
-		}
-		else
-		{
-			isIconResourceID = true;
-			iconIdentifier = GlobalData.PROFILE_ICON_DEFAULT;
-			profileName = getResources().getString(R.string.profile_name_default);
-			useCustomColor = false;
-		}
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+    }
 
-		Intent shortcutIntent = new Intent(getActivity().getApplicationContext(), BackgroundActivateProfileActivity.class);
-		// BackgroundActivateProfileActivity musi toto testovat, a len spravit aktivaciu profilu
-		shortcutIntent.putExtra(GlobalData.EXTRA_START_APP_SOURCE, GlobalData.STARTUP_SOURCE_SHORTCUT);
-		shortcutIntent.putExtra(GlobalData.EXTRA_PROFILE_ID, profile._id);
-		
-		Intent intent = new Intent();
-		intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
-		intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, profileName);
-		
-		int iconResource;
+    @Override
+    public void onDestroy()
+    {
+        if (!isAsyncTaskPendingOrRunning())
+        {
+            if (listView != null)
+                listView.setAdapter(null);
+            if (profileListAdapter != null)
+                profileListAdapter.release();
+
+            profileList = null;
+
+            if (dataWrapper != null)
+                dataWrapper.invalidateDataWrapper();
+            dataWrapper = null;
+        }
+
+        super.onDestroy();
+    }
+
+    private void createShortcut(int position)
+    {
+        Profile profile = profileList.get(position);
+        boolean isIconResourceID;
+        String iconIdentifier;
+        Bitmap profileBitmap;
+        Bitmap shortcutOverlayBitmap;
+        Bitmap profileShortcutBitmap;
+        String profileName;
+        boolean useCustomColor;
+
+        if (profile != null)
+        {
+            isIconResourceID = profile.getIsIconResourceID();
+            iconIdentifier = profile.getIconIdentifier();
+            profileName = profile._name;
+            useCustomColor = profile.getUseCustomColorForIcon();
+        }
+        else
+        {
+            isIconResourceID = true;
+            iconIdentifier = GlobalData.PROFILE_ICON_DEFAULT;
+            profileName = getResources().getString(R.string.profile_name_default);
+            useCustomColor = false;
+        }
+
+        Intent shortcutIntent = new Intent(getActivity().getApplicationContext(), BackgroundActivateProfileActivity.class);
+        // BackgroundActivateProfileActivity musi toto testovat, a len spravit aktivaciu profilu
+        shortcutIntent.putExtra(GlobalData.EXTRA_START_APP_SOURCE, GlobalData.STARTUP_SOURCE_SHORTCUT);
+        shortcutIntent.putExtra(GlobalData.EXTRA_PROFILE_ID, profile._id);
+
+        Intent intent = new Intent();
+        intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
+        intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, profileName);
+
         if (isIconResourceID)
         {
             if (profile._iconBitmap != null)
                 profileBitmap = profile._iconBitmap;
             else {
-                iconResource = getResources().getIdentifier(iconIdentifier, "drawable", getActivity().getPackageName());
+                int iconResource = getResources().getIdentifier(iconIdentifier, "drawable", getActivity().getPackageName());
                 profileBitmap = BitmapFactory.decodeResource(getResources(), iconResource);
             }
         }
@@ -217,16 +216,20 @@ public class ShortcutCreatorListFragment extends Fragment {
             int height = (int) resources.getDimension(android.R.dimen.app_icon_size);
             int width = (int) resources.getDimension(android.R.dimen.app_icon_size);
             profileBitmap = BitmapManipulator.resampleBitmap(iconIdentifier, width, height);
+            if (profileBitmap == null) {
+                int iconResource = R.drawable.ic_profile_default;
+                profileBitmap = BitmapFactory.decodeResource(getResources(), iconResource);
+            }
         }
         
         if (GlobalData.applicationWidgetIconColor.equals("1"))
         {
-    		int monochromeValue = 0xFF;
-    		if (GlobalData.applicationWidgetIconLightness.equals("0")) monochromeValue = 0x00;
-    		if (GlobalData.applicationWidgetIconLightness.equals("25")) monochromeValue = 0x40;
-    		if (GlobalData.applicationWidgetIconLightness.equals("50")) monochromeValue = 0x80;
-    		if (GlobalData.applicationWidgetIconLightness.equals("75")) monochromeValue = 0xC0;
-    		if (GlobalData.applicationWidgetIconLightness.equals("100")) monochromeValue = 0xFF;
+            int monochromeValue = 0xFF;
+            if (GlobalData.applicationWidgetIconLightness.equals("0")) monochromeValue = 0x00;
+            if (GlobalData.applicationWidgetIconLightness.equals("25")) monochromeValue = 0x40;
+            if (GlobalData.applicationWidgetIconLightness.equals("50")) monochromeValue = 0x80;
+            if (GlobalData.applicationWidgetIconLightness.equals("75")) monochromeValue = 0xC0;
+            if (GlobalData.applicationWidgetIconLightness.equals("100")) monochromeValue = 0xFF;
 
             if (isIconResourceID || useCustomColor) {
                 // icon is from resource or colored by custom color
@@ -235,33 +238,33 @@ public class ShortcutCreatorListFragment extends Fragment {
             else
                 profileBitmap = BitmapManipulator.grayscaleBitmap(profileBitmap);
         }
-		
-    	shortcutOverlayBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_shortcut_overlay);
-    	profileShortcutBitmap = combineImages(profileBitmap, shortcutOverlayBitmap);
-    	intent.putExtra(Intent.EXTRA_SHORTCUT_ICON, profileShortcutBitmap);
-				
-    	getActivity().setResult(Activity.RESULT_OK, intent);
-		
-		getActivity().finish();
-	}
-	
-	private Bitmap combineImages(Bitmap bitmap1, Bitmap bitmap2)
-	{
-		Bitmap combined;
-		
-		int width;
-		int height;
-		
-		width = bitmap1.getWidth();
-		height = bitmap1.getHeight();
-		
-		combined = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
 
-		Canvas canvas = new Canvas(combined);
-		canvas.drawBitmap(bitmap1, 0f, 0f, null);
-		canvas.drawBitmap(bitmap2, 0f, 0f, null);
+        shortcutOverlayBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_shortcut_overlay);
+        profileShortcutBitmap = combineImages(profileBitmap, shortcutOverlayBitmap);
+        intent.putExtra(Intent.EXTRA_SHORTCUT_ICON, profileShortcutBitmap);
 
-		return combined;
-	}
-	
+        getActivity().setResult(Activity.RESULT_OK, intent);
+
+        getActivity().finish();
+    }
+
+    private Bitmap combineImages(Bitmap bitmap1, Bitmap bitmap2)
+    {
+        Bitmap combined;
+
+        int width;
+        int height;
+
+        width = bitmap1.getWidth();
+        height = bitmap1.getHeight();
+
+        combined = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(combined);
+        canvas.drawBitmap(bitmap1, 0f, 0f, null);
+        canvas.drawBitmap(bitmap2, 0f, 0f, null);
+
+        return combined;
+    }
+
 }
