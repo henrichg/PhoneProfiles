7a3d20c24b5611f43f3c99f5656da5cff45f0e73
henrichg <henrich.gron@chello.sk>
2015-10-16 09:27:00 +0200

    New Material Dialogs lib version

--- phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/ActivateProfileListAdapter.java
+++ phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/ActivateProfileListAdapter.java
@@ -15,210 +15,210 @@ import java.util.List;
 public class ActivateProfileListAdapter extends BaseAdapter
 {
 
-	private List<Profile> profileList;
-	private Fragment fragment;
-	
-	public ActivateProfileListAdapter(Fragment f, List<Profile> pl)
-	{
-		fragment = f;
-		profileList = pl;
-	}   
-	
-	public void release()
-	{
-		fragment = null;
-		profileList = null;
-	}
-	
-	public int getCount()
-	{
-		return profileList.size();
-	}
+    private List<Profile> profileList;
+    private Fragment fragment;
 
-	public Object getItem(int position)
-	{
-		return profileList.get(position);
-	}
+    public ActivateProfileListAdapter(Fragment f, List<Profile> pl)
+    {
+        fragment = f;
+        profileList = pl;
+    }
 
-	public long getItemId(int position)
-	{
-		return position;
-	}
+    public void release()
+    {
+        fragment = null;
+        profileList = null;
+    }
 
-	public int getItemId(Profile profile)
-	{
-		for (int i = 0; i < profileList.size(); i++)
-		{
-			if (profileList.get(i)._id == profile._id)
-				return i;
-		}
-		return -1;
-	}
-	
-	public void setList(List<Profile> pl)
-	{
-		profileList = pl;
-		notifyDataSetChanged();
-	}
-	
-	public void addItem(Profile profile)
-	{
-		int maxPOrder = 0;
-		int pOrder;
-		for (Profile p : profileList)
-		{
-			pOrder = p._porder;
-			if (pOrder > maxPOrder) maxPOrder = pOrder;
-		}
-		profile._porder = maxPOrder+1;
-		profileList.add(profile);
-		notifyDataSetChanged();
-	}
+    public int getCount()
+    {
+        return profileList.size();
+    }
 
-	public void updateItem(Profile profile)
-	{
-		notifyDataSetChanged();
-	}
-	
-	public void deleteItem(Profile profile)
-	{
-		profileList.remove(profile);
-		notifyDataSetChanged();
-	}
-	
-	public void changeItemOrder(int from, int to)
-	{
-		Profile profile = profileList.get(from);
-		profileList.remove(from);
-		profileList.add(to, profile);
-		for (int i = 0; i < profileList.size(); i++)
-		{
-			profileList.get(i)._porder = i+1;
-		}
-		notifyDataSetChanged();
-	}
-	
-	public Profile getActivatedProfile()
-	{
-		for (Profile p : profileList)
-		{
-			if (p._checked)
-			{
-				return p;
-			}
-		}
-		
-		return null;
-	}
+    public Object getItem(int position)
+    {
+        return profileList.get(position);
+    }
 
-	static class ViewHolder {
-		  RelativeLayout listItemRoot;
-		  ImageView profileIcon;
-		  TextView profileName;
-		  ImageView profileIndicator;
-		  int position;
-		}
-	
-	public View getView(int position, View convertView, ViewGroup parent)
-	{
-		ViewHolder holder;
-		
-		View vi = convertView;
+    public long getItemId(int position)
+    {
+        return position;
+    }
+
+    public int getItemId(Profile profile)
+    {
+        for (int i = 0; i < profileList.size(); i++)
+        {
+            if (profileList.get(i)._id == profile._id)
+                return i;
+        }
+        return -1;
+    }
+
+    public void setList(List<Profile> pl)
+    {
+        profileList = pl;
+        notifyDataSetChanged();
+    }
+
+    public void addItem(Profile profile)
+    {
+        int maxPOrder = 0;
+        int pOrder;
+        for (Profile p : profileList)
+        {
+            pOrder = p._porder;
+            if (pOrder > maxPOrder) maxPOrder = pOrder;
+        }
+        profile._porder = maxPOrder+1;
+        profileList.add(profile);
+        notifyDataSetChanged();
+    }
+
+    public void updateItem(Profile profile)
+    {
+        notifyDataSetChanged();
+    }
+
+    public void deleteItem(Profile profile)
+    {
+        profileList.remove(profile);
+        notifyDataSetChanged();
+    }
+
+    public void changeItemOrder(int from, int to)
+    {
+        Profile profile = profileList.get(from);
+        profileList.remove(from);
+        profileList.add(to, profile);
+        for (int i = 0; i < profileList.size(); i++)
+        {
+            profileList.get(i)._porder = i+1;
+        }
+        notifyDataSetChanged();
+    }
+
+    public Profile getActivatedProfile()
+    {
+        for (Profile p : profileList)
+        {
+            if (p._checked)
+            {
+                return p;
+            }
+        }
+
+        return null;
+    }
+
+    static class ViewHolder {
+          RelativeLayout listItemRoot;
+          ImageView profileIcon;
+          TextView profileName;
+          ImageView profileIndicator;
+          int position;
+        }
+
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        ViewHolder holder;
+
+        View vi = convertView;
         if (convertView == null)
         {
             holder = new ViewHolder();
-    		LayoutInflater inflater = LayoutInflater.from(fragment.getActivity());
-			if (!GlobalData.applicationActivatorGridLayout)
-			{
-	        	if (GlobalData.applicationActivatorPrefIndicator)
-	        		vi = inflater.inflate(R.layout.activate_profile_list_item, parent, false);
-	        	else
-	        		vi = inflater.inflate(R.layout.activate_profile_list_item_no_indicator, parent, false);
-	            holder.listItemRoot = (RelativeLayout)vi.findViewById(R.id.act_prof_list_item_root);
-	            holder.profileName = (TextView)vi.findViewById(R.id.act_prof_list_item_profile_name);
-	            holder.profileIcon = (ImageView)vi.findViewById(R.id.act_prof_list_item_profile_icon);
-	    		if (GlobalData.applicationActivatorPrefIndicator)
-	    			holder.profileIndicator = (ImageView)vi.findViewById(R.id.act_prof_list_profile_pref_indicator);
-			}
-			else
-			{
-	      		vi = inflater.inflate(R.layout.activate_profile_grid_item, parent, false);
-		        holder.listItemRoot = (RelativeLayout)vi.findViewById(R.id.act_prof_list_item_root);
-		        holder.profileName = (TextView)vi.findViewById(R.id.act_prof_list_item_profile_name);
-		        holder.profileIcon = (ImageView)vi.findViewById(R.id.act_prof_list_item_profile_icon);
-			}
+            LayoutInflater inflater = LayoutInflater.from(fragment.getActivity());
+            if (!GlobalData.applicationActivatorGridLayout)
+            {
+                if (GlobalData.applicationActivatorPrefIndicator)
+                    vi = inflater.inflate(R.layout.activate_profile_list_item, parent, false);
+                else
+                    vi = inflater.inflate(R.layout.activate_profile_list_item_no_indicator, parent, false);
+                holder.listItemRoot = (RelativeLayout)vi.findViewById(R.id.act_prof_list_item_root);
+                holder.profileName = (TextView)vi.findViewById(R.id.act_prof_list_item_profile_name);
+                holder.profileIcon = (ImageView)vi.findViewById(R.id.act_prof_list_item_profile_icon);
+                if (GlobalData.applicationActivatorPrefIndicator)
+                    holder.profileIndicator = (ImageView)vi.findViewById(R.id.act_prof_list_profile_pref_indicator);
+            }
+            else
+            {
+                vi = inflater.inflate(R.layout.activate_profile_grid_item, parent, false);
+                holder.listItemRoot = (RelativeLayout)vi.findViewById(R.id.act_prof_list_item_root);
+                holder.profileName = (TextView)vi.findViewById(R.id.act_prof_list_item_profile_name);
+                holder.profileIcon = (ImageView)vi.findViewById(R.id.act_prof_list_item_profile_icon);
+            }
             vi.setTag(holder);        
         }
         else
         {
-        	holder = (ViewHolder)vi.getTag();
+            holder = (ViewHolder)vi.getTag();
         }
 
         Profile profile = profileList.get(position);
 
         if (profile._checked && (!GlobalData.applicationActivatorHeader))
         {
-        	if (GlobalData.applicationTheme.equals("material"))
-        		holder.listItemRoot.setBackgroundResource(R.drawable.header_card);
-        	else
-           	if (GlobalData.applicationTheme.equals("dark"))
-           		holder.listItemRoot.setBackgroundResource(R.drawable.header_card_dark);
-        	else
-           	if (GlobalData.applicationTheme.equals("dlight"))
-           		holder.listItemRoot.setBackgroundResource(R.drawable.header_card);
-        	holder.profileName.setTypeface(null, Typeface.BOLD);
+            if (GlobalData.applicationTheme.equals("material"))
+                holder.listItemRoot.setBackgroundResource(R.drawable.header_card);
+            else
+            if (GlobalData.applicationTheme.equals("dark"))
+                holder.listItemRoot.setBackgroundResource(R.drawable.header_card_dark);
+            else
+            if (GlobalData.applicationTheme.equals("dlight"))
+                holder.listItemRoot.setBackgroundResource(R.drawable.header_card);
+            holder.profileName.setTypeface(null, Typeface.BOLD);
         }
         else
         {
-        	if (GlobalData.applicationTheme.equals("material"))
-        		holder.listItemRoot.setBackgroundResource(R.drawable.card);
-        	else
-           	if (GlobalData.applicationTheme.equals("dark"))
-           		holder.listItemRoot.setBackgroundResource(R.drawable.card_dark);
-        	else
-           	if (GlobalData.applicationTheme.equals("dlight"))
-           		holder.listItemRoot.setBackgroundResource(R.drawable.card);
-        	holder.profileName.setTypeface(null, Typeface.NORMAL);
+            if (GlobalData.applicationTheme.equals("material"))
+                holder.listItemRoot.setBackgroundResource(R.drawable.card);
+            else
+            if (GlobalData.applicationTheme.equals("dark"))
+                holder.listItemRoot.setBackgroundResource(R.drawable.card_dark);
+            else
+            if (GlobalData.applicationTheme.equals("dlight"))
+                holder.listItemRoot.setBackgroundResource(R.drawable.card);
+            holder.profileName.setTypeface(null, Typeface.NORMAL);
         }
 
-		String profileName = profile.getProfileNameWithDuration();
+        String profileName = profile.getProfileNameWithDuration();
         holder.profileName.setText(profileName);
 
         if (profile.getIsIconResourceID())
         {
             if (profile._iconBitmap != null)
-				holder.profileIcon.setImageBitmap(profile._iconBitmap);
-			else {
-				holder.profileIcon.setImageResource(0);
-				int res = vi.getResources().getIdentifier(profile.getIconIdentifier(), "drawable",
-						vi.getContext().getPackageName());
-				holder.profileIcon.setImageResource(res); // resource na ikonu
-			}
+                holder.profileIcon.setImageBitmap(profile._iconBitmap);
+            else {
+                //holder.profileIcon.setImageBitmap(null);
+                int res = vi.getResources().getIdentifier(profile.getIconIdentifier(), "drawable",
+                        vi.getContext().getPackageName());
+                holder.profileIcon.setImageResource(res); // resource na ikonu
+            }
         }
         else
         {
-        	holder.profileIcon.setImageBitmap(profile._iconBitmap);
+            holder.profileIcon.setImageBitmap(profile._iconBitmap);
         }
 
-		if ((GlobalData.applicationActivatorPrefIndicator) && (!GlobalData.applicationActivatorGridLayout))
-		{
-			//profilePrefIndicatorImageView.setImageBitmap(null);
-			//Bitmap bitmap = ProfilePreferencesIndicator.paint(profile, vi.getContext());
-			//profilePrefIndicatorImageView.setImageBitmap(bitmap);
-			holder.profileIndicator.setImageBitmap(profile._preferencesIndicator);
-		}
+        if ((GlobalData.applicationActivatorPrefIndicator) && (!GlobalData.applicationActivatorGridLayout))
+        {
+            //profilePrefIndicatorImageView.setImageBitmap(null);
+            //Bitmap bitmap = ProfilePreferencesIndicator.paint(profile, vi.getContext());
+            //profilePrefIndicatorImageView.setImageBitmap(bitmap);
+            holder.profileIndicator.setImageBitmap(profile._preferencesIndicator);
+        }
 
         /*		ImageView profileItemEditMenu = (ImageView)vi.findViewById(R.id.act_prof_list_item_edit_menu);
-		profileItemEditMenu.setTag(position);
-		profileItemEditMenu.setOnClickListener(new OnClickListener() {
+        profileItemEditMenu.setTag(position);
+        profileItemEditMenu.setOnClickListener(new OnClickListener() {
 
-				public void onClick(View v) {
-					activity.openContextMenu(v);
-				}
-			});
+                public void onClick(View v) {
+                    activity.openContextMenu(v);
+                }
+            });
 */		
-			
-		return vi;
-	}
+
+        return vi;
+    }
 
 }
