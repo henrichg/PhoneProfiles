58c8ca9bb8b326267f30d088f449761ef698dfd7
henrichg <henrich.gron@chello.sk>
2016-02-08 15:23:32 +0100

    Optimized imports

--- phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/EditorProfileListFragment.java
+++ phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/EditorProfileListFragment.java
@@ -9,7 +9,6 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
