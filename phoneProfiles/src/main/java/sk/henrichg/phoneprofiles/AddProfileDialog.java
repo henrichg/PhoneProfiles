8bfc6954f00db9cfda80db6b0318d3ecb3c43df5
henrichg <henrich.gron@chello.sk>
2016-03-14 18:44:23 +0100

    Reverted: Changed MaterialDialog to AppCompatDialog

--- phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/AddProfileDialog.java
+++ phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/AddProfileDialog.java
@@ -1,11 +1,12 @@
 package sk.henrichg.phoneprofiles;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatDialog;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
+
+import com.afollestad.materialdialogs.MaterialDialog;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,8 +20,7 @@ public class AddProfileDialog
     private Context _context;
     private EditorProfileListFragment profileListFragment;
 
-    //private MaterialDialog mDialog;
-    private AppCompatDialog mDialog;
+    private MaterialDialog mDialog;
     private ListView listView;
 
     public AddProfileDialog(Context context, EditorProfileListFragment profileListFragment)
@@ -47,11 +47,15 @@ public class AddProfileDialog
             profileList.add(profile);
         }
 
-        mDialog = new AppCompatDialog(context);
-        mDialog.setTitle(R.string.new_profile_predefined_profiles_dialog);
-        mDialog.setContentView(R.layout.activity_profile_pref_dialog);
+        MaterialDialog.Builder dialogBuilder = new MaterialDialog.Builder(context)
+                            .title(R.string.new_profile_predefined_profiles_dialog)
+                            //.disableDefaultFonts()
+                            .autoDismiss(false)
+                            .customView(R.layout.activity_profile_pref_dialog, false);
 
-        listView = (ListView)mDialog.findViewById(R.id.profile_pref_dlg_listview);
+        mDialog = dialogBuilder.build();
+
+        listView = (ListView)mDialog.getCustomView().findViewById(R.id.profile_pref_dlg_listview);
 
         addProfileAdapter = new AddProfileAdapter(this, _context, profileList);
         listView.setAdapter(addProfileAdapter);
