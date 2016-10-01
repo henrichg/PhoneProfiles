a9456cb01b2b5e315c57b6918775f1859ad94bed
henrichg <henrich.gron@chello.sk>
2016-10-01 11:09:46 +0200

    ...

--- phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/PackageReplacedReceiver.java
+++ phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/PackageReplacedReceiver.java
@@ -6,25 +6,25 @@ import android.content.Intent;
 
 public class PackageReplacedReceiver extends BroadcastReceiver {
 
-	@Override
-	public void onReceive(Context context, Intent intent) {
+    @Override
+    public void onReceive(Context context, Intent intent) {
 
-		//int intentUid = intent.getExtras().getInt("android.intent.extra.UID");
-		//int myUid = android.os.Process.myUid();
-		//if (intentUid == myUid)
-		//{
-			GlobalData.loadPreferences(context);
-			
-			if (GlobalData.getApplicationStarted(context))
-			{
-				// must by false for avoiding starts/pause events before restart events
-				GlobalData.setApplicationStarted(context, false); 
-				
-				// start ReceiverService
-				context.startService(new Intent(context.getApplicationContext(), PhoneProfilesService.class));
-			}
+        //int intentUid = intent.getExtras().getInt("android.intent.extra.UID");
+        //int myUid = android.os.Process.myUid();
+        //if (intentUid == myUid)
+        //{
+            GlobalData.loadPreferences(context);
 
-		//}
-	}
+            if (GlobalData.getApplicationStarted(context))
+            {
+                // must by false for avoiding starts/pause events before restart events
+                GlobalData.setApplicationStarted(context, false);
+
+                // start ReceiverService
+                context.startService(new Intent(context.getApplicationContext(), PhoneProfilesService.class));
+            }
+
+        //}
+    }
 
 }
