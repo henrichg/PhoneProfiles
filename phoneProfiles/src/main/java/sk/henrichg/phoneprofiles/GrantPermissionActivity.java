b78ebdc0beb1eb3307fecfef4645c30d2eeb20bd
henrichg <henrich.gron@chello.sk>
2016-12-19 15:26:11 +0100

    ...

--- phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/GrantPermissionActivity.java
+++ phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/GrantPermissionActivity.java
@@ -389,6 +389,7 @@ public class GrantPermissionActivity extends Activity {
                         GlobalData.clearMergedPermissions(getApplicationContext());
                 }
                 //return;
+                break;
             }
 
             // other 'case' lines to check for other
