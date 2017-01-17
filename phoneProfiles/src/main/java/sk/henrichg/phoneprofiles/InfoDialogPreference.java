3a9572250b553f1d0b3eee8e1aabc66d257d5236
henrichg <henrich.gron@chello.sk>
2017-01-17 12:05:19 +0100

    Added info help into preferences about profile preference types (R), (S), (I).

---
+++ phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/InfoDialogPreference.java
@@ -0,0 +1,58 @@
+package sk.henrichg.phoneprofiles;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.preference.DialogPreference;
+import android.support.v7.app.AlertDialog;
+import android.util.AttributeSet;
+
+public class InfoDialogPreference extends DialogPreference {
+
+    private String infoText;
+
+    private Context _context;
+
+    public InfoDialogPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        _context = context;
+
+        TypedArray typedArray = context.obtainStyledAttributes(attrs,
+                R.styleable.InfoDialogPreference);
+
+        infoText = typedArray.getString(R.styleable.InfoDialogPreference_infoText);
+
+        typedArray.recycle();
+    }
+
+    @Override
+    protected void showDialog(Bundle state) {
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(_context);
+        dialogBuilder.setTitle(getDialogTitle());
+        dialogBuilder.setMessage(infoText);
+        dialogBuilder.setPositiveButton(android.R.string.ok, null);
+
+        GlobalGUIRoutines.registerOnActivityDestroyListener(this, this);
+
+        dialogBuilder.show();
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        super.onDismiss(dialog);
+        GlobalGUIRoutines.unregisterOnActivityDestroyListener(this, this);
+    }
+
+    @Override
+    protected void onSetInitialValue(boolean restoreValue, Object defaultValue)
+    {
+    }
+
+    public void setInfoText(String _infoText) {
+        this.infoText = _infoText;
+    }
+
+}
