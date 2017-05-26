eb83a20e1a58e074ce1388cbaf5451654abb64e8
henrichg <henrich.gron@chello.sk>
2017-05-26 08:21:48 +0200

    Fixed: Do not use PPApplication.sleep() in UI thread.

--- phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/PackageReplacedReceiver.java
+++ phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/PackageReplacedReceiver.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.os.Handler;
 
 import java.util.Calendar;
 
@@ -17,36 +18,38 @@ public class PackageReplacedReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         //Thread.setDefaultUncaughtExceptionHandler(new TopExceptionHandler());
 
+        final Context appContext = context.getApplicationContext();
+
         //int intentUid = intent.getExtras().getInt("android.intent.extra.UID");
         //int myUid = android.os.Process.myUid();
         //if (intentUid == myUid)
         //{
-            //PPApplication.loadPreferences(context);
+            //PPApplication.loadPreferences(appContext);
 
             // start delayed bootup broadcast
             PPApplication.startedOnBoot = true;
-            AlarmManager alarmMgr = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
-            Intent delayedBootUpIntent = new Intent(context, DelayedBootUpReceiver.class);
-            PendingIntent alarmIntent = PendingIntent.getBroadcast(context.getApplicationContext(), 0, delayedBootUpIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+            AlarmManager alarmMgr = (AlarmManager)appContext.getSystemService(Context.ALARM_SERVICE);
+            Intent delayedBootUpIntent = new Intent(appContext, DelayedBootUpReceiver.class);
+            PendingIntent alarmIntent = PendingIntent.getBroadcast(appContext, 0, delayedBootUpIntent, PendingIntent.FLAG_CANCEL_CURRENT);
             Calendar calendar = Calendar.getInstance();
             calendar.add(Calendar.SECOND, 10);
             alarmMgr.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), alarmIntent);
 
-            Permissions.setShowRequestAccessNotificationPolicyPermission(context.getApplicationContext(), true);
-            Permissions.setShowRequestWriteSettingsPermission(context.getApplicationContext(), true);
-            ActivateProfileHelper.setScreenUnlocked(context.getApplicationContext(), true);
+            Permissions.setShowRequestAccessNotificationPolicyPermission(appContext, true);
+            Permissions.setShowRequestWriteSettingsPermission(appContext, true);
+            ActivateProfileHelper.setScreenUnlocked(appContext, true);
 
-            int oldVersionCode = PPApplication.getSavedVersionCode(context.getApplicationContext());
+            int oldVersionCode = PPApplication.getSavedVersionCode(appContext);
             PPApplication.logE("@@@ PackageReplacedReceiver.onReceive", "oldVersionCode="+oldVersionCode);
             int actualVersionCode;
             try {
-                PackageInfo pinfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+                PackageInfo pinfo = appContext.getPackageManager().getPackageInfo(appContext.getPackageName(), 0);
                 actualVersionCode = pinfo.versionCode;
                 PPApplication.logE("@@@ PackageReplacedReceiver.onReceive", "actualVersionCode=" + actualVersionCode);
 
                 if (oldVersionCode < actualVersionCode) {
                     if (actualVersionCode <= 2100) {
-                        ApplicationPreferences.getSharedPreferences(context);
+                        ApplicationPreferences.getSharedPreferences(appContext);
                         SharedPreferences.Editor editor = ApplicationPreferences.preferences.edit();
                         editor.putBoolean(ActivateProfileActivity.PREF_START_TARGET_HELPS, false);
                         editor.putBoolean(ActivateProfileListFragment.PREF_START_TARGET_HELPS, false);
@@ -62,22 +65,32 @@ public class PackageReplacedReceiver extends BroadcastReceiver {
                 //e.printStackTrace();
             }
 
-            if (PPApplication.getApplicationStarted(context, false))
+            if (PPApplication.getApplicationStarted(appContext, false))
             {
                 if (PhoneProfilesService.instance != null) {
                     // stop PhoneProfilesService
-                    context.stopService(new Intent(context.getApplicationContext(), PhoneProfilesService.class));
-                    PPApplication.sleep(2000);
+                    appContext.stopService(new Intent(appContext, PhoneProfilesService.class));
+                    Handler handler = new Handler();
+                    Runnable r = new Runnable() {
+                        public void run() {
+                            startService(appContext);
+                        }
+                    };
+                    handler.postDelayed(r, 2000);
                 }
-
-                // must by false for avoiding starts/pause events before restart events
-                PPApplication.setApplicationStarted(context, false);
-
-                // start ReceiverService
-                context.startService(new Intent(context.getApplicationContext(), PhoneProfilesService.class));
+                else
+                    startService(appContext);
             }
 
         //}
+    }
+
+    private void startService(Context context) {
+        // must by false for avoiding starts/pause events before restart events
+        PPApplication.setApplicationStarted(context, false);
+
+        // start PhoneProfilesService
+        context.startService(new Intent(context.getApplicationContext(), PhoneProfilesService.class));
     }
 
 }
