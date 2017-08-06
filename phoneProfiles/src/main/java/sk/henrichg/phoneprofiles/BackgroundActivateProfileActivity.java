5db0e2db15a6233239cb42f04868e2109b6bfa1e
henrichg <henrich.gron@chello.sk>
2017-08-06 12:57:59 +0200

    Optimized imports.

--- phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/BackgroundActivateProfileActivity.java
+++ phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/BackgroundActivateProfileActivity.java
@@ -2,7 +2,6 @@ package sk.henrichg.phoneprofiles;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 
 public class BackgroundActivateProfileActivity extends Activity {
