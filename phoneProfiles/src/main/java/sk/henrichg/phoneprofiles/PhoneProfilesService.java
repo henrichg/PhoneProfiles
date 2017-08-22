8a78d865083fdc86f57a622221e7fc0bb6e787e7
henrichg <henrich.gron@chello.sk>
2017-08-22 20:01:10 +0200

    Optimized imports.

--- phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/PhoneProfilesService.java
+++ phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/PhoneProfilesService.java
@@ -13,7 +13,6 @@ import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
 import android.telephony.TelephonyManager;
-import android.util.Log;
 
 import com.commonsware.cwac.wakeful.WakefulIntentService;
 import com.crashlytics.android.Crashlytics;
