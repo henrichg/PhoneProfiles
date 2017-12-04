29c336b25a59d5166ae845a8682283ce98d5649d
henrichg <henrich.gron@chello.sk>
2017-12-04 21:36:49 +0100

    Optimized imports.

--- phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/LockDeviceActivity.java
+++ phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/LockDeviceActivity.java
@@ -3,7 +3,6 @@ package sk.henrichg.phoneprofiles;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
-import android.os.Handler;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
