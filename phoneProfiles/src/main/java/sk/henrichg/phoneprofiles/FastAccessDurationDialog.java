cd60279d1dce2dd485c6c3ccafcb839b67c95ed3
henrichg <henrich.gron@chello.sk>
2018-01-29 20:54:38 +0100

    Removed neutral buttons from dialogs.

--- phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/FastAccessDurationDialog.java
+++ phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/FastAccessDurationDialog.java
@@ -6,6 +6,7 @@ import android.content.DialogInterface;
 import android.support.annotation.NonNull;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.Spinner;
@@ -78,9 +79,8 @@ class FastAccessDurationDialog implements SeekBar.OnSeekBarChangeListener{
 
         MaterialDialog.Builder mBuilder = new MaterialDialog.Builder(mActivity)
                 .title(mActivity.getString(R.string.profile_preferences_duration))
-                .positiveText(R.string.fast_access_duration_activate_with_button)
+                .positiveText(android.R.string.ok)
                 .negativeText(android.R.string.cancel)
-                .neutralText(R.string.fast_access_duration_activate_without_button)
                 .customView(R.layout.activity_fast_access_duration_dialog, true)
                 .onPositive(new MaterialDialog.SingleButtonCallback() {
                     @Override
@@ -111,20 +111,6 @@ class FastAccessDurationDialog implements SeekBar.OnSeekBarChangeListener{
                     public void onClick(@NonNull MaterialDialog materialDialog, @NonNull DialogAction dialogAction) {
                         updateEndsTimer = null;
                         mDataWrapper.finishActivity(mStartupSource, false, mActivity);
-                    }
-                })
-                .onNeutral(new MaterialDialog.SingleButtonCallback() {
-                    @Override
-                    public void onClick(@NonNull MaterialDialog materialDialog, @NonNull DialogAction dialogAction) {
-                        updateEndsTimer = null;
-
-                        mProfile._duration = 0;
-                        DatabaseHandler.getInstance(mDataWrapper.context).updateProfile(mProfile);
-
-                        if (Permissions.grantProfilePermissions(mActivity, mProfile, false,
-                                true, mMonochrome, mMonochromeValue,
-                                mStartupSource, /*true,*/ mActivity, true))
-                            mDataWrapper._activateProfile(mProfile, mStartupSource, /*true,*/ mActivity);
                     }
                 })
                 .dismissListener(new DialogInterface.OnDismissListener() {
@@ -270,6 +256,24 @@ class FastAccessDurationDialog implements SeekBar.OnSeekBarChangeListener{
                 }
             }
         }.init(activity), 250, 250);
+
+        final Button activateWithoutButton = layout.findViewById(R.id.fast_access_duration_dlg_activate_without);
+        activateWithoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateEndsTimer = null;
+
+                mProfile._duration = 0;
+                DatabaseHandler.getInstance(mDataWrapper.context).updateProfile(mProfile);
+
+                if (Permissions.grantProfilePermissions(mActivity, mProfile, false,
+                        true, mMonochrome, mMonochromeValue,
+                        mStartupSource, /*true,*/ mActivity, true))
+                    mDataWrapper._activateProfile(mProfile, mStartupSource, /*true,*/ mActivity);
+
+                mDialog.dismiss();
+            }
+        });
 
     }
 
