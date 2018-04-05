77a922e84158cf7ef655f4812b8ca82934067e67
henrichg <henrich.gron@chello.sk>
2018-04-05 10:03:34 +0200

    Removed ColorNotificationIcon class.

--- phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/PhoneProfilesService.java
+++ phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/PhoneProfilesService.java
@@ -17,6 +17,7 @@ import android.content.pm.PackageInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.drawable.Icon;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
@@ -583,7 +584,7 @@ public class PhoneProfilesService extends Service {
                                 (android.os.Build.VERSION.SDK_INT == 23);
                         //Log.d("ActivateProfileHelper.showNotification","isNote4="+isNote4);
                         if ((android.os.Build.VERSION.SDK_INT >= 23) && (!isNote4)) {
-                            notificationBuilder.setSmallIcon(ColorNotificationIcon.getFromBitmap(iconBitmap));
+                            notificationBuilder.setSmallIcon(Icon.createWithBitmap(iconBitmap));
                         }
                         else {
                             iconSmallResource = dataWrapper.context.getResources().getIdentifier(iconIdentifier + "_notify_color", "drawable", dataWrapper.context.getPackageName());
@@ -640,7 +641,7 @@ public class PhoneProfilesService extends Service {
                         (android.os.Build.VERSION.SDK_INT == 23);
                 //Log.d("ActivateProfileHelper.showNotification","isNote4="+isNote4);
                 if ((Build.VERSION.SDK_INT >= 23) && (!isNote4) && (iconBitmap != null)) {
-                    notificationBuilder.setSmallIcon(ColorNotificationIcon.getFromBitmap(iconBitmap));
+                    notificationBuilder.setSmallIcon(Icon.createWithBitmap(iconBitmap));
                 }
                 else {
                     int iconSmallResource;
