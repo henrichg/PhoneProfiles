622426750cd3e33ff3b4a899e3fb71df84c82457
henrichg <henrich.gron@chello.sk>
2018-04-18 21:17:33 +0200

    Fixed ANR: at sk.henrichg.phoneprofiles.WifiSSIDData.fillWifiConfigurationList (WifiSSIDData.java:44)              at sk.henrichg.phoneprofiles.WifiStateChangedBroadcastReceiver.onReceive (WifiStateChangedBroadcastReceiver.java:32)

--- phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/WifiStateChangedBroadcastReceiver.java
+++ phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/WifiStateChangedBroadcastReceiver.java
@@ -5,8 +5,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
+import android.os.Handler;
+import android.os.PowerManager;
 
 import java.util.List;
+
+import static android.content.Context.POWER_SERVICE;
 
 public class WifiStateChangedBroadcastReceiver extends BroadcastReceiver {
 
@@ -14,45 +18,55 @@ public class WifiStateChangedBroadcastReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         PPApplication.logE("##### WifiStateChangedBroadcastReceiver.onReceive", "xxx");
 
-        //WifiManager wifi = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        final Context appContext = context.getApplicationContext();
 
-        if (!PPApplication.getApplicationStarted(context, true))
+        if (!PPApplication.getApplicationStarted(appContext, true))
             // application is not started
             return;
 
-        //loadPreferences(context);
-
-        int wifiState = intent.getIntExtra(WifiManager.EXTRA_WIFI_STATE, 0);
+        final int wifiState = intent.getIntExtra(WifiManager.EXTRA_WIFI_STATE, 0);
         PPApplication.logE("$$$ WifiStateChangedBroadcastReceiver.onReceive","state="+wifiState);
 
         if ((wifiState == WifiManager.WIFI_STATE_ENABLED) || (wifiState == WifiManager.WIFI_STATE_DISABLED))
         {
-            if (wifiState == WifiManager.WIFI_STATE_ENABLED) {
-                // refresh configured networks list
-                WifiSSIDData.fillWifiConfigurationList(context);
+            PPApplication.startHandlerThread();
+            final Handler handler = new Handler(PPApplication.handlerThread.getLooper());
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    PowerManager powerManager = (PowerManager) appContext.getSystemService(POWER_SERVICE);
+                    PowerManager.WakeLock wakeLock = null;
+                    if (powerManager != null) {
+                        wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "WifiStateChangedBroadcastReceiver.onReceive");
+                        wakeLock.acquire(10 * 60 * 1000);
+                    }
 
-                if (!PhoneProfilesService.connectToSSID.equals(Profile.CONNECTTOSSID_JUSTANY)) {
-                    WifiManager wifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-                    if (wifiManager != null) {
-                        List<WifiConfiguration> list = wifiManager.getConfiguredNetworks();
-                        if (list != null) {
-                            for (WifiConfiguration i : list) {
-                                if (i.SSID != null && i.SSID.equals(PhoneProfilesService.connectToSSID)) {
-                                    //wifiManager.disconnect();
-                                    wifiManager.enableNetwork(i.networkId, true);
-                                    //wifiManager.reconnect();
-                                    break;
+                    if (wifiState == WifiManager.WIFI_STATE_ENABLED) {
+                        // refresh configured networks list
+                        WifiSSIDData.fillWifiConfigurationList(appContext);
+
+                        if (!PhoneProfilesService.connectToSSID.equals(Profile.CONNECTTOSSID_JUSTANY)) {
+                            WifiManager wifiManager = (WifiManager) appContext.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+                            if (wifiManager != null) {
+                                List<WifiConfiguration> list = wifiManager.getConfiguredNetworks();
+                                if (list != null) {
+                                    for (WifiConfiguration i : list) {
+                                        if (i.SSID != null && i.SSID.equals(PhoneProfilesService.connectToSSID)) {
+                                            //wifiManager.disconnect();
+                                            wifiManager.enableNetwork(i.networkId, true);
+                                            //wifiManager.reconnect();
+                                            break;
+                                        }
+                                    }
                                 }
                             }
                         }
                     }
+
+                    if ((wakeLock != null) && wakeLock.isHeld())
+                        wakeLock.release();
                 }
-                //else {
-                //    WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-                //    wifiManager.disconnect();
-                //    wifiManager.reconnect();
-                //}
-            }
+            });
         }
 
     }
