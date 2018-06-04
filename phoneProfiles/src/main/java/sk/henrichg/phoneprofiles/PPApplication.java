6cecb7e52b5425cbf8d76518ce2c8dc28072241b
henrichg <henrich.gron@chello.sk>
2018-06-04 13:26:51 +0200

    Removed logs.

--- phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/PPApplication.java
+++ phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/PPApplication.java
@@ -59,7 +59,7 @@ public class PPApplication extends Application {
     public static final String EXPORT_PATH = "/PhoneProfiles";
     private static final String LOG_FILENAME = "log.txt";
 
-    private static final boolean logIntoLogCat = true;
+    private static final boolean logIntoLogCat = false;
     private static final boolean logIntoFile = false;
     private static final boolean rootToolsDebug = false;
     private static final String logFilterTags = "##### PPApplication.onCreate"
@@ -75,7 +75,7 @@ public class PPApplication extends Application {
                                             //+"|ProfileDurationAlarmBroadcastReceiver"
 
                                             //+"|ActivateProfilesHelper.executeForForceStopApplications"
-                                            +"|DataWrapper.setDynamicLauncherShortcuts"
+                                            //+"|DataWrapper.setDynamicLauncherShortcuts"
             ;
 
     static final String EXTRA_PROFILE_ID = "profile_id";
