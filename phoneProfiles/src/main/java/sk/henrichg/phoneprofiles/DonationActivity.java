ab11efcdb439f4f7f9918e6249b3e299e69cab64
henrichg <henrich.gron@chello.sk>
2018-06-20 16:17:24 +0200

    Optimized imports.

--- phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/DonationActivity.java
+++ phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/DonationActivity.java
@@ -2,7 +2,6 @@ package sk.henrichg.phoneprofiles;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
