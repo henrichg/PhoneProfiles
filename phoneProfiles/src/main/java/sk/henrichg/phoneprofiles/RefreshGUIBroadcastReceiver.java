c46498573a4043e63232d3e32e2d87fae2ad74b2
henrichg <henrich.gron@chello.sk>
2018-07-31 10:54:19 +0200

    Removed Activator and Editor instance.

--- phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/RefreshGUIBroadcastReceiver.java
+++ phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/RefreshGUIBroadcastReceiver.java
@@ -3,26 +3,33 @@ package sk.henrichg.phoneprofiles;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
 
 public class RefreshGUIBroadcastReceiver extends BroadcastReceiver {
 
-    private static final String EXTRA_REFRESH_ICONS = "refresh_icons";
-    public static final String EXTRA_REFRESH_ALSO_EDITOR = "refresh_also_editor";
+    static final String EXTRA_REFRESH_ICONS = "refresh_icons";
+    static final String EXTRA_REFRESH_ALSO_EDITOR = "refresh_also_editor";
 
     @Override
     public void onReceive(Context context, Intent intent) {
         boolean refreshIcons = intent.getBooleanExtra(EXTRA_REFRESH_ICONS, false);
         boolean refreshAlsoEditor = intent.getBooleanExtra(EXTRA_REFRESH_ALSO_EDITOR, true);
 
-        ActivateProfileActivity activateProfileActivity = ActivateProfileActivity.getInstance();
+        Intent refreshIntent = new Intent("RefreshActivatorGUIBroadcastReceiver");
+        refreshIntent.putExtra(EXTRA_REFRESH_ICONS, refreshIcons);
+        LocalBroadcastManager.getInstance(context).sendBroadcast(refreshIntent);
+        /*ActivateProfileActivity activateProfileActivity = ActivateProfileActivity.getInstance();
         if (activateProfileActivity != null)
-            activateProfileActivity.refreshGUI(refreshIcons);
+            activateProfileActivity.refreshGUI(refreshIcons);*/
 
         if (refreshAlsoEditor) {
-            EditorProfilesActivity editorProfilesActivity = EditorProfilesActivity.getInstance();
+            refreshIntent = new Intent("RefreshEditorGUIBroadcastReceiver");
+            refreshIntent.putExtra(EXTRA_REFRESH_ICONS, refreshIcons);
+            LocalBroadcastManager.getInstance(context).sendBroadcast(refreshIntent);
+            /*EditorProfilesActivity editorProfilesActivity = EditorProfilesActivity.getInstance();
             if (editorProfilesActivity != null)
                 // not change selection in editor if refresh is outside editor
-                editorProfilesActivity.refreshGUI(refreshIcons, false);
+                editorProfilesActivity.refreshGUI(refreshIcons, false);*/
         }
     }
 
