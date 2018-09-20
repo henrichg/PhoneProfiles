05c31878c825e15e01de2a3276af4f23660cc77f
henrichg <henrich.gron@chello.sk>
2018-09-20 09:46:42 +0200

    Fixed unlink volumes settings.

--- phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/PhoneProfilesPreferencesNestedFragment.java
+++ phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/PhoneProfilesPreferencesNestedFragment.java
@@ -87,17 +87,18 @@ public class PhoneProfilesPreferencesNestedFragment extends PreferenceFragment
         preferences = prefMng.getSharedPreferences();
         preferences.registerOnSharedPreferenceChangeListener(this);
 
-        PreferenceScreen systemCategory = (PreferenceScreen) findPreference("categorySystem");
+        //PreferenceScreen systemCategory = (PreferenceScreen) findPreference("categorySystem");
         if (!ActivateProfileHelper.getMergedRingNotificationVolumes(getActivity().getApplicationContext())) {
             Preference preference = findPreference(ApplicationPreferences.PREF_APPLICATION_UNLINK_RINGER_NOTIFICATION_VOLUMES);
             if (preference != null)
-                systemCategory.removePreference(preference);
+                preference.setEnabled(false);
+                //systemCategory.removePreference(preference);
         }
-        else {
+        /*else {
             Preference preference = findPreference(ApplicationPreferences.PREF_APPLICATION_RINGER_NOTIFICATION_VOLUMES_UNLINKED_INFO);
             if (preference != null)
                 systemCategory.removePreference(preference);
-        }
+        }*/
 
         /*if (Build.VERSION.SDK_INT >= 24) {
             PreferenceScreen preferenceCategory = (PreferenceScreen) findPreference("applicationInterfaceCategory");
