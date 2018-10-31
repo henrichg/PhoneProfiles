3c36af5a63e776e08e399d46d34b71777fa4c117
henrichg <henrich.gron@chello.sk>
2018-10-31 13:49:08 +0100

    Added border in widgets. (3)

--- phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/PhoneProfilesPreferencesNestedFragment.java
+++ phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/PhoneProfilesPreferencesNestedFragment.java
@@ -743,6 +743,25 @@ public class PhoneProfilesPreferencesNestedFragment extends PreferenceFragment
             }
         }
 
+        if (key.equals(ApplicationPreferences.PREF_APPLICATION_WIDGET_ICON_SHOW_BORDER)) {
+            Preference _preference = prefMng.findPreference(ApplicationPreferences.PREF_APPLICATION_WIDGET_ICON_LIGHTNESS_BORDER);
+            if (_preference != null) {
+                _preference.setEnabled(preferences.getBoolean(key, false));
+            }
+        }
+        if (key.equals(ApplicationPreferences.PREF_APPLICATION_WIDGET_ONE_ROW_SHOW_BORDER)) {
+            Preference _preference = prefMng.findPreference(ApplicationPreferences.PREF_APPLICATION_WIDGET_ONE_ROW_LIGHTNESS_BORDER);
+            if (_preference != null) {
+                _preference.setEnabled(preferences.getBoolean(key, false));
+            }
+        }
+        if (key.equals(ApplicationPreferences.PREF_APPLICATION_WIDGET_LIST_SHOW_BORDER)) {
+            Preference _preference = prefMng.findPreference(ApplicationPreferences.PREF_APPLICATION_WIDGET_LIST_LIGHTNESS_BORDER);
+            if (_preference != null) {
+                _preference.setEnabled(preferences.getBoolean(key, false));
+            }
+        }
+
         // Do not bind toggles.
         if (preference instanceof CheckBoxPreference || preference instanceof TwoStatePreference) {
             return;
