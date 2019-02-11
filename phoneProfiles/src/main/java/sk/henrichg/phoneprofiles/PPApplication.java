6edd17c37927ec85710c518cd7522c920cc62330
henrichg <henrich.gron@chello.sk>
2019-02-11 13:54:23 +0100

    ...

--- phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/PPApplication.java
+++ phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/PPApplication.java
@@ -67,7 +67,8 @@ public class PPApplication extends Application {
     public static final String EXPORT_PATH = "/PhoneProfiles";
     private static final String LOG_FILENAME = "log.txt";
 
-    private static final boolean logIntoLogCat = false;
+    @SuppressWarnings("PointlessBooleanExpression")
+    private static final boolean logIntoLogCat = true && BuildConfig.DEBUG;
     static final boolean logIntoFile = false;
     private static final boolean rootToolsDebug = false;
     private static final String logFilterTags = "##### PPApplication.onCreate"
