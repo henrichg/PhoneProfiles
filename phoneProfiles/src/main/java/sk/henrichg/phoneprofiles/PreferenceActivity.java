fc4fc764302ef56005a39e8c47d5378deb3f963f
henrichg <henrich.gron@chello.sk>
2019-03-04 23:37:44 +0100

    Fixed some toolbar elevation.

--- phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/PreferenceActivity.java
+++ phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/PreferenceActivity.java
@@ -49,8 +49,11 @@ public abstract class PreferenceActivity extends AppCompatPreferenceActivity
         toolbar = findViewById(R.id.mp_toolbar);
         setSupportActionBar(toolbar);
 
-        getSupportActionBar().setHomeButtonEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setHomeButtonEnabled(true);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setElevation(GlobalGUIRoutines.dpToPx(1));
+        }
     }
 
     @Override
