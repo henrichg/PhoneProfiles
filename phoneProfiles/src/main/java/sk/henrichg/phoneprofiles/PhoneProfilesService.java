fe3f58942203a3dd4a4df22fb40c207a17daf8d3
henrichg <henrich.gron@chello.sk>
2019-10-01 14:34:59 +0200

    Maybe fixed FC: android.app.RemoteServiceException: Bad notification for startForeground: java.lang.RuntimeException: invalid channel for service notification: Notification(channel=phoneProfiles_activated_profile pri=0 contentView=sk.henrichg.phoneprofiles/0x7f0c0088 vibrate=null sound=null defaults=0x0 flags=0x6a color=0xff237e9f category=status vis=PUBLIC).

--- phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/PhoneProfilesService.java
+++ phoneProfiles/src/main/java/sk/henrichg/phoneprofiles/PhoneProfilesService.java
@@ -877,18 +877,13 @@ public class PhoneProfilesService extends Service {
                 preferencesIndicator = null;
             }
 
-            notificationBuilder = new Notification.Builder(appContext);
-            notificationBuilder.setContentIntent(pIntent);
-
-            //if (Build.VERSION.SDK_INT >= 21)
-                notificationBuilder.setColor(ContextCompat.getColor(appContext, R.color.notificationDecorationColor));
-
             if (Build.VERSION.SDK_INT >= 26) {
-                PPApplication.createProfileNotificationChannel(/*profile, */appContext);
-                notificationBuilder.setChannelId(PPApplication.PROFILE_NOTIFICATION_CHANNEL);
+                PPApplication.createProfileNotificationChannel(appContext);
+                notificationBuilder = new Notification.Builder(appContext, PPApplication.PROFILE_NOTIFICATION_CHANNEL);
                 //notificationBuilder.setSettingsText("Test");
             }
             else {
+                notificationBuilder = new Notification.Builder(appContext);
                 if (notificationShowInStatusBar) {
                     KeyguardManager myKM = (KeyguardManager) appContext.getSystemService(Context.KEYGUARD_SERVICE);
                     if (myKM != null) {
@@ -906,11 +901,10 @@ public class PhoneProfilesService extends Service {
                     notificationBuilder.setPriority(Notification.PRIORITY_MIN);
             }
 
-            //if (android.os.Build.VERSION.SDK_INT >= 21)
-            //{
-                notificationBuilder.setCategory(Notification.CATEGORY_STATUS);
-                notificationBuilder.setVisibility(Notification.VISIBILITY_PUBLIC);
-            //}
+            notificationBuilder.setContentIntent(pIntent);
+            notificationBuilder.setColor(ContextCompat.getColor(appContext, R.color.notificationDecorationColor));
+            notificationBuilder.setCategory(Notification.CATEGORY_STATUS);
+            notificationBuilder.setVisibility(Notification.VISIBILITY_PUBLIC);
 
             //notificationBuilder.setTicker(profileName);
 
